524fe301a79d002975a6328a5ccd3d276447d290
102-26 <102-26@102-26>
2023-07-08 18:42:00 +0300

    Add project files.

---
+++ ETICARET.DAL/Abstract/IProductDal.cs
@@ -0,0 +1,17 @@
+﻿using ETICARET.ENTITY;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETICARET.DAL.Abstract
+{
+    public interface IProductDal
+    {
+        Product GetById(int id);
+        Product GetOne(Expression<Func<Product,bool>> filter); //i => i.Id==id
+        IQueryable<Product> GetAll(Expression<Func<Product, bool>> filter);
+    }
+}
---
+++ ETICARET.ENTITY/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETICARET.ENTITY
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Image> Images { get; set; }
+        public decimal Price { get; set; }
+
+        public List<ProductCategory> ProductCategories { get; set; }
+    }
+}
